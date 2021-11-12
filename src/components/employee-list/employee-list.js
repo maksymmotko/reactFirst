db6f9b676e039a33726ebe22039b6b6af548f345
Maksym <maksym.motko@gmail.com>
2021-11-12 23:23:49 +0100

    commit

---
+++ src/components/employee-list/employee-list.js
@@ -0,0 +1,25 @@
+import EmployeeListItem from '../employee-list-item/employee-list-item';
+
+import './employee-list.css';
+
+const EmployeeList = ({data, onDelete, onToggleProp}) => {
+
+    const elements = data.map(item => {
+        const {id, ...itemProps} = item;
+        return (
+            <EmployeeListItem 
+            key={id} 
+            {...itemProps}
+            onDelete={() => onDelete(id)}
+            onToggleProp={(e) => onToggleProp(id, e.currentTarget.getAttribute('data-toggle'))}/>   //name={item.name} salary={item.salary}
+        )
+    })
+
+    return (
+        <ul className="app-list list-group">
+            {elements}
+        </ul>
+    );
+}
+
+export default EmployeeList;
